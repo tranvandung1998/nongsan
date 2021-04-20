56bd518d0eb8a712bf076846dd9130abd0e851ce
unknown <tvdung182898@gmail.com>
2021-04-20 07:54:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var images = [
+    asset/monan/background1.jpg
+    asset/monan/background2.jpg
+]
+var num = 0;
+
+function next() {
+    var slider = document.getElementById("slider");
+    num++;
+    if(num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
+
+function prev() {
+    var slider = document.getElementById("slider");
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
+setInterval
